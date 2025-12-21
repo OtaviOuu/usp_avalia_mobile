8b768a44f92429bf80157d7641b762db7d44e648
otaviouu <otavio.venancio.oliveira@usp.br>
2025-12-21 19:09:34 -0300

    update ShowDisciplinaScreen to display disciplinaCodigo

--- app/(tabs)/disciplinas/[codigo].tsx
+++ app/(tabs)/disciplinas/[codigo].tsx
@@ -1,9 +1,12 @@
+import { useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function ShowDisciplinaScreen() {
+  const disciplinaCodigo = useLocalSearchParams<{ codigo: string }>().codigo;
+
   return (
     <View>
-      <Text>Show Disciplina Screen</Text>
+      <Text>Show Disciplina {disciplinaCodigo}</Text>
     </View>
   );
 }
